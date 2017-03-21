9191ef15d703dfb1b7e74ddc9afc7bd7bc8e3510
Walter <relw1wa-github@yahoo.de>
2017-03-21 12:10:24 +0100

    Getting Started with Redux: Create react dumb component and pass store methods and value to it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -13,49 +17,33 @@ const counter = (state = 0, action) => {
     - store.getState(): get current state
     - store.dispatch(): change state by providing a action
     - store.subscribe(): act upon state changes by subscribing */
-const createStore = (reducer) => {
-  let state;
-  let listeners = [];
-
-  const getState = () => state;
-
-  const dispatch = (action) => {
-    state = reducer(state, action);
-    // - each listener gets called (they are functions)
-    // - state is not delivered, listener has to fetch it via getState
-    listeners.forEach(listener => listener());
-  };
-
-  const subscribe = (listener) => {
-    // add new listener who subscribed to listeners
-    listeners.push(listener);
-    // - provide unsubscribe functionality without having to create
-    //   explicit unsubscribe method within store
-    // - to unsubscribe, store subscription and call it to unsubscribe:
-    //   let subscription = store.subscribe(subscriber);
-    //   subscription() -> unsubscribed
-    return () => {
-      listeners = listeners.filter(l => l !== listener);
-    }
-  };
-
-  // dummy action to get initial value from reducer
-  dispatch({});
-
-  return { getState, dispatch, subscribe }
-}
-
 const store = createStore(counter);
 
-// refactor subscription
+// react functional component
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
 const render = () => {
-  document.body.innerText = store.getState();
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    document.getElementById('root')
+  );
 }
+
 store.subscribe(render);
 
 // render initial value
 render();
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
-});
