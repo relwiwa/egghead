d2f626bc6fa0fefc67d0da91ea0d2d961992f818
Walter <relw1wa-github@yahoo.de>
2017-03-21 11:58:30 +0100

    Getting Started with Redux: Rebuild createStore and understand its functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { createStore } from 'redux';
-
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -15,6 +13,38 @@ const counter = (state = 0, action) => {
     - store.getState(): get current state
     - store.dispatch(): change state by providing a action
     - store.subscribe(): act upon state changes by subscribing */
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    // - each listener gets called (they are functions)
+    // - state is not delivered, listener has to fetch it via getState
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    // add new listener who subscribed to listeners
+    listeners.push(listener);
+    // - provide unsubscribe functionality without having to create
+    //   explicit unsubscribe method within store
+    // - to unsubscribe, store subscription and call it to unsubscribe:
+    //   let subscription = store.subscribe(subscriber);
+    //   subscription() -> unsubscribed
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    }
+  };
+
+  // dummy action to get initial value from reducer
+  dispatch({});
+
+  return { getState, dispatch, subscribe }
+}
+
 const store = createStore(counter);
 
 // refactor subscription
