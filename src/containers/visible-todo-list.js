b92e332f20bb9dc069265c88a334717c42bd17e2
Walter <relw1wa-github@yahoo.de>
2018-09-23 18:32:44 +0100

    Transform VisibleTodoList into connected container component

--- src/containers/visible-todo-list.js
+++ src/containers/visible-todo-list.js
@@ -1,51 +1,43 @@
-import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import TodoList from '../components/todo-list';
 
-class VisibleTodoList extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    // subscribe returns way to unsubscribe
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_COMPLETED':
+      return todos.filter(
+        t => t.completed
+      );
+    case 'SHOW_ACTIVE':
+      return todos.filter(
+        t => !t.completed
+      );
   }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-  
-  getVisibleTodos(todos, filter) {
-    switch (filter) {
-      case 'SHOW_ALL':
-        return todos;
-      case 'SHOW_COMPLETED':
-        return todos.filter(
-          t => t.completed
-        );
-      case 'SHOW_ACTIVE':
-        return todos.filter(
-          t => !t.completed
-        );
-    }
-  };
-  
-  render() {
-    const { store } = this.context;
-    const state = store.getState();
-    return <TodoList
-      todos={this.getVisibleTodos(
-        state.todos,
-        state.visibilityFilter
-      )}
-      onTodoClick={id => store.dispatch({
-        type: 'TOGGLE_TODO',
-        id
-      })}
-    />
-  }
-}
-
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object,
 };
+
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
+    ),
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: id => dispatch({
+      type: 'TOGGLE_TODO',
+      id
+    }),
+  };
+};
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList);
 
 export default VisibleTodoList;
