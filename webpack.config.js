804ce108f9c0ca9bd2ca45667e69b25a0bb70ec4
Walter <relw1wa-github@yahoo.de>
2018-10-16 11:09:26 +0200

    Setup configuration for usage of React with Typescript

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/app.tsx',
+  output: {
+    path: __dirname + '/public',
+    filename: 'build/app.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+};
