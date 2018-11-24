41b5491724ef365dc11c61c5ebde1801ef5d565d
Walter <relw1wa-github@yahoo.de>
2018-11-24 12:36:19 +0100

    Add components for bookshelf app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
+import Home from "./components/Home";
+import BookDetail from "./components/BookDetail";
+
+import "./App.css";
+
+const App = (props) => {
+  return (
+    <Router>
       <div className="App">
-        App
+        <div className="App-header">
+          <div className="Inner">
+            <Link to="/" className="Logo">
+              <img
+                href="https://rawgit.com/eggheadio-projects/add-internationalization-i18n-to-a-react-app-using-react-intl/006d466ca1fca8f9bd772390a2d5b5e421676186/01-install-and-configure-react-intl/src/books.svg"
+                alt="Logo"
+              />
+              <h2>On My Shelf</h2>
+            </Link>
+          </div>
+        </div>
+
+        <div className="Container Inner">
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/book/:bookId' component={BookDetail}/>
+          </Switch>
+        </div>
       </div>
-    );
-  }
+    </Router>
+  );
 }
 
 export default App;
