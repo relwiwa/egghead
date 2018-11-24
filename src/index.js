2ec909c5800cbdf250a5bd57feb66c0034bf0e18
Walter <relw1wa-github@yahoo.de>
2018-11-24 11:44:21 +0100

    Install and setup react-intl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import en from 'react-intl/locale-data/en';
+import fr from 'react-intl/locale-data/fr';
+import es from 'react-intl/locale-data/es';
+
 import App from './App';
+import messages from './messages';
+
+addLocaleData([...en, ...es, ...fr]);
+
+const locale = (navigator.languages && navigator.languages[0])
+  || navigator.language
+  || navigator.userLanguage
+  || 'en-US';
 
 ReactDOM.render(
-  <App />,
+  <IntlProvider locale={locale} messages={messages[locale]}>
+    <App />
+  </IntlProvider>,
   document.getElementById('root')
 );
