7c3f50fbe3cea8f47937bc694892d6bbc7c378d3
Walter <relw1wa-github@yahoo.de>
2018-11-25 12:47:52 +0100

    Add polyfill for intl

--- src/index.js
+++ src/index.js
@@ -9,20 +9,40 @@ import App from './App';
 import { flattenMessages } from './utils';
 import messages from './messages';
 
-addLocaleData([...en, ...es, ...fr]);
-
-let locale = (navigator.languages && navigator.languages[0])
-  || navigator.language
-  || navigator.userLanguage
-  || 'en-US';
-
-if (messages[locale] === undefined) {
-  locale = 'en-US';
+if (!window.Intl) {
+  require.ensure([
+    'intl',
+    'intl/locale-data/jsonp/en.js',
+    'intl/locale-data/jsonp/es.js',
+    'intl/locale-data/jsonp/fr.js',
+  ], (require) => {
+    require('intl');
+    require('intl/locale-data/jsonp/en.js');
+    require('intl/locale-data/jsonp/es.js');
+    require('intl/locale-data/jsonp/fr.js');
+  });
+  runApp();
+}
+else {
+  runApp();
 }
 
-ReactDOM.render(
-  <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
-    <App />
-  </IntlProvider>,
-  document.getElementById('root')
-);
+function runApp() {
+  addLocaleData([...en, ...es, ...fr]);
+
+  let locale = (navigator.languages && navigator.languages[0])
+    || navigator.language
+    || navigator.userLanguage
+    || 'en-US';
+
+  if (messages[locale] === undefined) {
+    locale = 'en-US';
+  }
+
+  ReactDOM.render(
+    <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+      <App />
+    </IntlProvider>,
+    document.getElementById('root')
+  );
+}
