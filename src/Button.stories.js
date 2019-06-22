8ae441fd81ed3c1d7b81c77c705ca70f1414f4e9
Walter <relw1wa-github@yahoo.de>
2019-06-22 10:57:22 +0200

    Make storybook components dynamic by using knobs addon

--- src/Button.stories.js
+++ src/Button.stories.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { text } from '@storybook/addon-knobs/react';
 
 import { Button } from './Button';
 
@@ -8,5 +9,5 @@ storiesOf('Button', module)
   <Button bg="palegoldenrod">Hello world</Button>
 ))
 .addWithJSX('with background 2', () => (
-  <Button bg="lightgreen">Hello world 2</Button>
+  <Button bg={text('bg', 'lightgreen')}>Hello world 2</Button>
 ));
